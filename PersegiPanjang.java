d115091e3d2b1ff38f59a9d56e799fe137c2c416
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-10-14 20:11:00 +0700

    Add files via upload

---
+++ PersegiPanjang.java
@@ -0,0 +1,10 @@
+package TugasPBO_3_Muhammad_Faizal;
+
+public class PersegiPanjang {
+    public static void main(String[] args) {
+        int panjang = 50,lebar = 45;
+        int luasPP = panjang * lebar;
+        
+        System.out.println("Luas Persegi Panjang : " + luasPP + "cm");
+    }
+}
