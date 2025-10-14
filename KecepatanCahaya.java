614cd9e30c164777b069d3d5ff7563a326783090
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-10-14 20:12:32 +0700

    Add files via upload

---
+++ KecepatanCahaya.java
@@ -0,0 +1,23 @@
+package TugasPBO_3_Muhammad_Faizal;
+
+public class HitungWaktuCahaya {
+
+    public static void main(String[] args) {
+        double jarakBumiKeBulan = 384400; 
+        double jarakMatahariKeBumi = 152100000;
+        double kecepatanCahaya = 300000;
+
+        double waktuBumiBulan = jarakBumiKeBulan / kecepatanCahaya;
+        double waktuMatahariBumiDetik = jarakMatahariKeBumi / kecepatanCahaya;
+
+        double waktuMatahariBumiMenit = waktuMatahariBumiDetik / 60;
+
+        System.out.println("--- Waktu Tempuh Cahaya ---");
+        System.out.println("Jarak Bumi ke Bulan: " + jarakBumiKeBulan + " km");
+        System.out.println("Waktu tempuh dari Bumi ke Bulan: " + waktuBumiBulan + " detik");
+        System.out.println("\n----------------------------------\n");
+        System.out.println("Jarak Matahari ke Bumi: " + jarakMatahariKeBumi + " km");
+        System.out.println("Waktu tempuh dari Matahari ke Bumi: " + waktuMatahariBumiDetik + " detik");
+        System.out.println("Atau setara dengan: " + waktuMatahariBumiMenit + " menit");
+    }
+}
