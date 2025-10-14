0b6f132fe70f2da4b950ba7d2499ae7cbbc3986c
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-10-14 20:10:41 +0700

    Add files via upload

---
+++ Phytagoras.java
@@ -0,0 +1,14 @@
+package TugasPBO_3_Muhammad_Faizal;
+
+public class Pythagoras {
+    public static void main(String[] args) {
+        double a = 6, b = 8;
+        double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        System.out.println("a : 6");
+        System.out.println("b : 8");
+        System.out.println("Rumus Pythagoras : c^2 = a^2 + b^2");
+        System.out.println("Nilai C : " + c);
+        System.out.println("");
+        System.out.println("Nilai Keliling Segitiga : " + (a + b + c));
+    }
+}
