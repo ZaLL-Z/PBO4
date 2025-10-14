a51ec1b30fa34e825f71fd663e2c358568376427
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-10-14 20:13:05 +0700

    Add files via upload

---
+++ ConvertCase.java
@@ -0,0 +1,11 @@
+package TugasPBO_3_Muhammad_Faizal;
+
+public class ConvertCase {
+    public static void main(String[] args) {
+        String a = "Saya Belajar Java";
+        String b = "Saya Belajar Java";
+        
+        System.out.println(a.toLowerCase());
+        System.out.println(a.toUpperCase());
+    }
+}
