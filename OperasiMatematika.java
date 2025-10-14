7f03a9b38bbe68138d26d37a586155a3720c8536
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-10-14 20:11:21 +0700

    Add files via upload

---
+++ OperasiMatematika.java
@@ -0,0 +1,23 @@
+package TugasPBO_3_Muhammad_Faizal;
+
+public class OperasiMatematika {
+    static void Pertambahan(int a, int b){
+        System.out.println("Pertambahan dari " + a + " + " + b + " = " + (a+b));
+    }
+    static void Pengurangan(int a, int b){
+        System.out.println("Pengurangan dari " + a + " - " + b + " = " + (a-b));
+    }
+    static void Perkalian(int a, int b){
+        System.out.println("Perkalian dari " + a + " x " + b + " = " + (a*b));
+    }
+    
+    public static void main(String[] args) {
+        int x = 22, y = 33;
+//        Pertambahan
+        Pertambahan(x, y);
+//        Pengurangan
+        Pengurangan(x, y);
+//        Perkalian
+        Perkalian(x, y);
+    }
+}
