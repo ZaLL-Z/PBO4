8bfa626d385722556357769056ab3d8f9361ab97
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-10-14 20:12:47 +0700

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,23 @@
+package TugasPBO_3_Muhammad_Faizal;
+
+public class Increment {
+    public static void main(String[] args) {
+//        Bilangan Asal
+        int a = 5;
+        int b = 7;
+        
+        
+//        Increment Prefix
+        System.out.println("Bilangan asal : " + a);
+        System.out.println("Increment Prefix : "+ ++a);
+        System.out.println("Hasil Prefix : "+ a);
+        
+        System.out.println("");
+        
+//        Increment Postfix
+        System.out.println("Bilangan asal : "+b);
+        System.out.println("Increment Postfix : "+ b++);
+        System.out.println("Hasil Postfix : "+ b);
+    }
+ 
+}
