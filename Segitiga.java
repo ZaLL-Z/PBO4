3b908c0ae2251518de5922d15831c556c3619108
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-10-14 20:10:18 +0700

    Add files via upload

---
+++ Segitiga.java
@@ -0,0 +1,16 @@
+package TugasPBO_3_Muhammad_Faizal;
+
+public class Segitiga {
+    public static void main(String[] args) {
+        int a = 6, t = 8;
+        double luas_segi3 = 0.5 * a * t;
+        
+        System.out.println("Menghitung Luas Alas Segitiga");
+        System.out.println("a = 6");
+        System.out.println("b = 8");
+        System.out.println("Rumus : 1/2 * a * t");
+        System.out.println("Hasilnya : " + luas_segi3);
+        
+    }
+ 
+}
