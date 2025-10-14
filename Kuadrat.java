240b9afc49c68adcc637a25a2ac07970b69ca68f
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-10-14 20:11:54 +0700

    Add files via upload

---
+++ Kuadrat.java
@@ -0,0 +1,14 @@
+package TugasPBO_3_Muhammad_Faizal;
+
+public class PersamaanKuadrat {
+    public static void main(String[] args) {
+        int a = 2, b = 10, c = 5;
+        int hasil = (b*b) - (4*a*c);
+        
+        System.out.println("Persamaan Kuadrat dari b^2 - 4ac");
+        System.out.println("b = " + b);
+        System.out.println("a = " + a);
+        System.out.println("c = " + c);
+        System.out.println("Hasil : " + hasil);
+    }
+}
