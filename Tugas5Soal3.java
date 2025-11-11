ff890c202a464469795a11e5a02f04bf961165e3
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-11-11 18:33:00 +0700

    Add files via upload

---
+++ Tugas5Soal3.java
@@ -0,0 +1,24 @@
+
+package tugasprak;
+
+import java.util.Scanner;
+
+
+public class Tugas5Soal3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan banyak baris: ");
+        int baris = input.nextInt();
+        
+        int angka = 1;
+        for(int i = 1; i <= baris; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.println(angka + "");
+                angka++;
+            }
+            System.out.println();
+        }
+
+    }
+}
