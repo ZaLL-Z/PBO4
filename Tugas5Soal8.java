c00bc27db978bd81e69ec0e507fe561e61f1f2ea
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-11-11 18:35:06 +0700

    Add files via upload

---
+++ Tugas5Soal8.java
@@ -0,0 +1,14 @@
+
+package tugasprak;
+
+
+public class Tugas5Soal8 {
+    public static void main(String[] args) {
+         int[] angka = {10, 20, 30, 40, 50};
+
+        for (int nilai : angka){
+            System.out.println(nilai);
+        }
+
+    }
+}
