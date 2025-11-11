fac637cb2dd54b3cd5ba82bd7397543a520dfb4d
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-11-11 18:34:32 +0700

    Add files via upload

---
+++ Tugas5Soal7.java
@@ -0,0 +1,20 @@
+
+package tugasprak;
+
+
+public class Tugas5Soal7 {
+    public static void main(String[] args) {
+         int i = 0;
+        System.out.println("Contoh i++:");
+        while (i < 5) {
+            System.out.println(i++); // nilai ditampilkan dulu baru ditambah
+        }
+
+        int j = 0;
+        System.out.println("\nContoh ++i:");
+        while (j < 5){
+            System.out.println(++j);
+        }
+
+    }
+}
