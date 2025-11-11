b20e998f27d1abfbf6efc813686a5baf69043f46
Muhammad Faizal💀 <oppaikunoichi@gmail.com>
2025-11-11 18:33:57 +0700

    Add files via upload

---
+++ Tugas5Soal6.java
@@ -0,0 +1,14 @@
+
+package tugasprak;
+
+
+public class Tugas5Soal6 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++){
+            if (i == 10 - 1 ){
+                System.out.println(11);
+                System.out.println(11);
+            }
+        }
+    }
+}
